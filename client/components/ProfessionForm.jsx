558638a2a96507101c5c6bdbe94e22c1f38293eb
sherratt-pemberton <Sherratt285@gmail.com>
2022-08-11 11:58:01 +1200

    Changed form fields from headings to inputs

--- client/components/ProfessionForm.jsx
+++ client/components/ProfessionForm.jsx
@@ -11,34 +11,39 @@ return (
     <form onSubmit={handleSubmit} className = 'form-body'>
       <h1>Your Profession</h1>
   
-      <h2>Income</h2>
-    
-      <h3>Salary</h3>
-      <h3>Interest</h3>
-      <h3>Dividends</h3>
-      <h3>Real Estate/Businesses</h3>
+      <h2>Income</h2>  
+      <input type = "number">Salary</input>
+      <input type = "number">Interest</input>
+      <input type = "number">Dividends</input>
+      <input type = "number">Real Estate/Businesses</input>
       <h4>(Interests + Dividends + Real Estate/Businesses is thge sum of your passive income)</h4>
-      <h3>Passive Income</h3>
-      <h3>Total Income</h3>
+      <input type = "number">Passive Income</input>
+      <input type = "number">Total Income</input>
 
       <h2>Expences</h2>
-      <h3>Taxes</h3>
-      <h3>Morgage/Rent Pay</h3>
-      <h3>School Loan Pay</h3>
-      <h3>Car Payment</h3>
-      <h3>Credit Card Payment</h3>
-      <h3>Retail Payment</h3>
-      <h3>Other Expences</h3>
-      <h3>Child Expences</h3>
+      <input type = "number">Taxes</input>
+      <input type = "number">Morgage/Rent Pay</input>
+      <input type = "number">School Loan Pay</input>
+      <input type = "number">Car Payment</input>
+      <input type = "number">Credit Card Payment</input>
+      <input type = "number">Retail Payment</input>
+      <input type = "number">Other Expences</input>
+      <input type = "number">Child Expences</input>
 
-      <h3>Per Child Expence</h3>
-      <h3>Total Expences</h3>
+      <input type = "number">Per Child Expence</input>
+      <input type = "number">Total Expences</input>
 
-      <h3>Monthly Cashflow</h3>
+      <input type = "number">Monthly Cashflow</input>
 
       <h2>Assets</h2>
 
       <h2>Liabilities</h2>
+      <input type = "number">Morgage</input>
+      <input type = "number">School Loans</input>
+      <input type = "number">Car Loans</input>
+      <input type = "number">Credit Cards</input>
+      <input type = "number">Retail Dept</input>
+
     </form>
   
   </div>
